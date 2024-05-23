359ebf96b5c4d41364e0bf052d838497da5317f8
= <=>
2024-05-23 05:13:32 +0000

    first version of hello world

---
+++ helloworld.c
@@ -0,0 +1,14 @@
+/*
+Author: Akashdeep Gupta
+Hello world
+DOC: 23rs May 2024
+*/
+
+#include <stdio.h>
+
+int main() {
+   /* my first program in C */
+   printf("Hello, World! \n");
+
+   return 0;
+}
